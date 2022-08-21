461a63c855e07f4485595a08f93080910ed869d6
silmu <svetlana.raitina@gmail.com>
2022-08-21 13:57:25 +0300

    Add 404 page

---
+++ src/pages/ErrorPage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const ErrorPage = () => {
+  return (
+    <div className='errorPage'>
+      <h1>Error! Page not found.</h1>
+      <p>4</p>
+      <p>0</p>
+      <p>4</p>
+    </div>
+  );
+};
+
+export default ErrorPage;
